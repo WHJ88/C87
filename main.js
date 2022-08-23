f90eb9ded58cfdd02b003a5481b0941d619ac34c
WHJ88 <109901083+WHJ88@users.noreply.github.com>
2022-08-23 09:32:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+
+  
+
+
+ctx.__beginPath();
+ctx.___strokeStyle____ = "grey";
+ctx.___lineWidth____ = 1;
+ctx.____rect()(x="150", y="143", width="430", height="200");
+ctx.stroke();
+
+/*
+    Use beginPath() function to begin a path.
+    Then, use strokeStyle to set the color to "blue".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x = 150 and y = 143
+with 40 as radius, 0 startAngle and 2 * Math.PI endAngle.
+*/
+ctx.__beginPath();
+ctx.___strokeStyle____ = "blue";
+ctx.___lineWidth____ = 5 ;
+ctx.____(x="150", y="143",r="40", startAngle="0",endAngle="2*Math.PI");
+ctx.stroke();
+
+
+
+// Similarly, create a black circle with position 350 and 210
+ctx.__beginPath();
+ctx.___strokeStyle____ = "black";
+ctx.___lineWidth____ = 5 ;
+ctx.____(x="350", y="210",r="40", startAngle="0",endAngle="2*Math.PI");
+ctx.stroke();
+// Similarly, create a red circle with position 210 and 40
+ctx.__beginPath();
+ctx.___strokeStyle____ = "red";
+ctx.___lineWidth____ = 5 ;
+ctx.____(x="210", y="40",r="40", startAngle="0",endAngle="2*Math.PI");
+ctx.stroke();
+// Similarly, create an orange circle with position 300 and 250
+ctx.__beginPath();
+ctx.___strokeStyle____ = "orange";
+ctx.___lineWidth____ = 5 ;
+ctx.____(x="300", y="250",r="40", startAngle="0",endAngle="2*Math.PI");
+ctx.stroke();
+// Similarly, create a green circle with position 400 and 250
+ctx.__beginPath();
+ctx.___strokeStyle____ = "green";
+ctx.___lineWidth____ = 5 ;
+ctx.____(x="400", y="250",r="40", startAngle="0",endAngle="2*Math.PI");
+ctx.stroke();
